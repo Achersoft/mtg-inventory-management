8fb20dd54c0cdfa74afe4237db3e490d394ed78c
achersoft <sr_wvu@hotmail.com>
2015-10-17 00:54:13 -0400

    fixed card import

--- server/src/main/java/com/achersoft/mtg/importer/CardImporterServiceImpl.java
+++ server/src/main/java/com/achersoft/mtg/importer/CardImporterServiceImpl.java
@@ -5,7 +5,9 @@ import com.achersoft.mtg.importer.dao.Sets;
 import com.achersoft.mtg.importer.persistence.ImporterMapper;
 import javax.inject.Inject;
 import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public class CardImporterServiceImpl implements CardImporterService {
     
     private @Inject ImporterMapper mapper;
@@ -15,16 +17,16 @@ public class CardImporterServiceImpl implements CardImporterService {
         sets.getSets().stream().filter((set) -> !set.onlineOnly).forEach((set) -> {
             set.setId(DigestUtils.sha1Hex(set.name));
             mapper.addSet(set);
-            set.getCards().stream().forEach((card) -> {
+            set.getCards().stream().map((card) -> {
                 if(card.names != null) {
                     Card splitCard = new Card();
                     splitCard.setId(DigestUtils.sha1Hex(String.join("", card.names)));
                     splitCard.setSetId(set.getId());
                     if(card.getType().contains("Instant") || card.getType().contains("Sorcery")) {
-                        if(card.names.size() <= 2)
-                            splitCard.setName(String.join(" // ", card.names));
-                        else
-                            splitCard.setName(String.join("/", card.names));
+                    if(card.names.size() <= 2)
+                        splitCard.setName(String.join(" // ", card.names));
+                    else
+                        splitCard.setName(String.join("/", card.names));
                     } else
                         splitCard.setName(String.join(" | ", card.names));
                     card.setSplitSequence(card.names.indexOf(card.name)+1);
@@ -32,6 +34,8 @@ public class CardImporterServiceImpl implements CardImporterService {
                     mapper.addCard(splitCard);
                 }
                 card.setSetId(set.getId());
+                return card;
+            }).forEach((card) -> {
                 mapper.addCard(card);
             });
         });
